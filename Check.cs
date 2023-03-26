60a54d16e6dd67252cc84868d9a8f2c812b756a3
Sunil Reddy <thisissunil999@gmail.com>
2023-03-26 16:35:59 -0400

    Update Check.cs

--- Check.cs
+++ Check.cs
@@ -1,97 +1,37 @@
 using Amazon;
+using Amazon.AutoScaling;
+using Amazon.AutoScaling.Model;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 
-// Set up the Amazon DynamoDB client
-var client = new AmazonDynamoDBClient();
+// Set up the AWS credentials and region
+var credentials = new BasicAWSCredentials(accessKey, secretKey);
+var clientConfig = new AmazonDynamoDBConfig { RegionEndpoint = RegionEndpoint.USEast1 };
 
-// Define the table schema
-var tableRequest = new CreateTableRequest
+// Create a new DynamoDB client
+var dynamoClient = new AmazonDynamoDBClient(credentials, clientConfig);
+
+// Define the table name
+var tableName = "my-table";
+
+// Enable auto scaling for the table
+var autoscalingClient = new AmazonAutoScalingClient(credentials, clientConfig);
+var request = new PutScalingPolicyRequest
 {
-    TableName = "myTable",
-    KeySchema = new List<KeySchemaElement>
+    PolicyName = "MyScalingPolicy",
+    ServiceNamespace = "dynamodb",
+    ResourceId = $"table/{tableName}",
+    ScalableDimension = "dynamodb:table:ReadCapacityUnits",
+    PolicyType = "TargetTrackingScaling",
+    TargetTrackingScalingPolicyConfiguration = new TargetTrackingScalingPolicyConfiguration
     {
-        new KeySchemaElement("id", KeyType.HASH)
-    },
-    AttributeDefinitions = new List<AttributeDefinition>
-    {
-        new AttributeDefinition("id", ScalarAttributeType.S)
-    },
-    BillingMode = BillingMode.PAY_PER_REQUEST,
-    GlobalSecondaryIndexes = new List<GlobalSecondaryIndex>
-    {
-        new GlobalSecondaryIndex
+        TargetValue = 50,
+        PredefinedMetricSpecification = new PredefinedMetricSpecification
         {
-            IndexName = "myIndex",
-            KeySchema = new List<KeySchemaElement>
-            {
-                new KeySchemaElement("name", KeyType.HASH)
-            },
-            Projection = new Projection
-            {
-                ProjectionType = ProjectionType.ALL
-            },
-            ProvisionedThroughput = new ProvisionedThroughput
-            {
-                ReadCapacityUnits = 5,
-                WriteCapacityUnits = 5
-            }
-        }
-    },
-    ProvisionedThroughput = new ProvisionedThroughput
-    {
-        ReadCapacityUnits = 5,
-        WriteCapacityUnits = 5
-    }
-};
-
-// Add autoscaling for read and write capacity
-tableRequest.SSESpecification = new SSESpecification
-{
-    Enabled = true
-};
-
-tableRequest.GlobalSecondaryIndexes[0].ProvisionedThroughput.AutoScalingSettings = new AutoScalingSettings
-{
-    MinimumUnits = 1,
-    MaximumUnits = 100,
-    AutoScalingDisabled = false
-};
-
-tableRequest.GlobalSecondaryIndexes[0].ProvisionedThroughput.AutoScalingSettings.ScaleInCooldown = 60;
-tableRequest.GlobalSecondaryIndexes[0].ProvisionedThroughput.AutoScalingSettings.ScaleOutCooldown = 60;
-
-tableRequest.ProvisionedThroughput.AutoScalingSettings = new AutoScalingSettings
-{
-    MinimumUnits = 1,
-    MaximumUnits = 100,
-    AutoScalingDisabled = false
-};
-
-tableRequest.ProvisionedThroughput.AutoScalingSettings.ScaleInCooldown = 60;
-tableRequest.ProvisionedThroughput.AutoScalingSettings.ScaleOutCooldown = 60;
-
-// Create the table
-var response = client.CreateTableAsync(tableRequest).Result;
-
-// Wait for the table to be created
-client.WaitUntilTableExistsAsync("myTable").Wait();
-
-// Create a global table
-var globalTableRequest = new CreateGlobalTableRequest
-{
-    GlobalTableName = "myGlobalTable",
-    ReplicationGroup = new List<Replica>
-    {
-        new Replica
-        {
-            RegionName = RegionEndpoint.USWest1.SystemName
-        },
-        new Replica
-        {
-            RegionName = RegionEndpoint.EUWest1.SystemName
+            PredefinedMetricType = "DynamoDBReadCapacityUtilization"
         }
     }
 };
 
-var globalTableResponse = client.CreateGlobalTableAsync(globalTableRequest).Result;
+
+var response = await autoscalingClient.PutScalingPolicyAsync(request);
